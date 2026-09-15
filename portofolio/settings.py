d6e0c2cc754f8125e7381312c9fb12346b33f3c0
Debora <deboraputridionsimamora@gmail.com>
2026-09-15 23:16:14 +0700

    Tutorial 3: Form dan Data Delivery

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -153,3 +153,6 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+# gunakan https:// untuk trailing urlnya
+CSRF_TRUSTED_ORIGINS = ["<https://debora-putri-myportofolio.pws.cs.ui.ac.id/>"]
